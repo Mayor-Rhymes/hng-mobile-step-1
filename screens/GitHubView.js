acba7c8175c589faec837d9c7113cfb7c10d14b5
Olakunle Raheem <olaks.raheem@gmail.com>
2023-09-09 08:26:39 +0100

    Chore: Changed build file

--- screens/GitHubView.js
+++ screens/GitHubView.js
@@ -3,7 +3,8 @@ import { WebView } from "react-native-webview";
 export default function GitHubView() {
   return (
     <WebView
-      source={{ uri: "http://github.com/mayor-rhymes" }}
+      originWhitelist={['*']}
+      source={{ uri: "https://github.com/mayor-rhymes" }}
       style={{ flex: 1 }}
     />
   );
